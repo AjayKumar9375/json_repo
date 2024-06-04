221aa57636a4866b5fc04ec45bea3585be2dd374
AjayKumar9375 <93271770+AjayKumar9375@users.noreply.github.com>
2024-06-04 18:58:19 +0530

    Update search3.py

--- search3.py
+++ search3.py
@@ -5,8 +5,12 @@ def parse_input_arguments():
     parser = argparse.ArgumentParser(description="inputs for script")
     parser.add_argument("--path_to_source_json_file", help="Provide the path to the source json file.", required=True,)
     parser.add_argument("--path_to_destination_json_file", help="Provide the path to the destination json file.", required=True,)
-    parser.add_argument("--location_to_be_searched", type=str, help="Provide the location to be searched in json file.", required=True)
-
+    # parser.add_argument("--location_to_be_searched", type=str, help="Provide the location to be searched in json file.", required=True)
+    
+    parser.add_argument("--location1", type=str, help="Provide the location to be searched in json file.", required=True)
+    parser.add_argument("--location2", type=str, help="Provide the location to be searched in json file.", required=True)
+    
+    location_to_be_searched = [location1,location2]
     
     args = parser.parse_args()
 
