f9748ba4c9beab163be796fd0b0f06f2c24a1d65
AjayKumar9375 <93271770+AjayKumar9375@users.noreply.github.com>
2024-06-04 19:22:16 +0530

    Update search3.py

--- search3.py
+++ search3.py
@@ -14,6 +14,6 @@ def parse_input_arguments():
 if __name__ == "__main__":
     configuration = parse_input_arguments()
     json_reader = JsonReader(configuration.path_to_source_json_file, configuration.path_to_destination_json_file)
-    # print(f"\n {json_reader.search_and_dump_data(configuration.location1, configuration.location2)}")
+    print(f"\n {json_reader.search_and_dump_data(configuration.location_to_be_searche)}")
     print(configuration.location_to_be_searched)
 
