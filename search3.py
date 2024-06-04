35cea2983a62ae91496d5beea8728e0f6eab9150
AjayKumar9375 <93271770+AjayKumar9375@users.noreply.github.com>
2024-06-04 22:05:25 +0530

    Update search3.py

--- search3.py
+++ search3.py
@@ -5,8 +5,8 @@ def parse_input_arguments():
     parser = argparse.ArgumentParser(description="inputs for script")
     parser.add_argument("--path_to_source_json_file", help="Provide the path to the source json file.", required=True,)
     parser.add_argument("--path_to_destination_json_file", help="Provide the path to the destination json file.", required=True,)
-    parser.add_argument("--location1", type=str, help="Provide the location to be searched in json file.", required=True)
-    parser.add_argument("--location2", type=str, help="Provide the location to be searched in json file.", required=True)
+    parser.add_argument("--location_one", type=str, help="Provide the location to be searched in json file.", required=True)
+    parser.add_argument("--location_two", type=str, help="Provide the location to be searched in json file.", required=True)
     args = parser.parse_args()
     return args
 
@@ -14,6 +14,6 @@ def parse_input_arguments():
 if __name__ == "__main__":
     configuration = parse_input_arguments()
     json_reader = JsonReader(configuration.path_to_source_json_file, configuration.path_to_destination_json_file)
-    print(f"\n {json_reader.search_and_dump_data(configuration.location1, configuration.location2)}")
+    print(f"\n {json_reader.search_and_dump_data(configuration.location_one, configuration.location_two)}")
     # print(configuration.location_to_be_searched)
 
