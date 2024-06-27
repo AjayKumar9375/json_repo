04fbb1b43016491aef680e4f391aaebeefc770de
Ajay Kumar <ajaya9375@gmail.com>
2024-06-27 19:06:57 +0530

    changes

--- .teamcity/buildTypes/build1.kt
+++ .teamcity/buildTypes/build1.kt
@@ -65,3 +65,9 @@ class DslTesting_BuildTypes(
 
 })
 
+object HttpsGithubComAjayKumar9375jsonRepoGitRefsHeadsMain : GitVcsRoot({
+    name = "https://github.com/AjayKumar9375/json_repo.git#refs/heads/main"
+    url = "https://github.com/AjayKumar9375/json_repo.git"
+    branch = "refs/heads/main"
+    branchSpec = "refs/heads/*"
+})
