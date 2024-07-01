c061f333061ee90b214f7a6705655a487671151e
Ajay Kumar <ajaya9375@gmail.com>
2024-07-01 08:18:26 +0530

    changes

--- .teamcity/vcs/vcsSetting.kt
+++ .teamcity/vcs/vcsSetting.kt
@@ -1,5 +1,7 @@
 package vcs.vcsSetting
 
+import jetbrains.buildServer.configs.kotlin.BuildType
+
 fun BuildType.vcsSetting(){
     vcs {
         root(RelativeId("HttpsGithubComAjayKumar9375jsonRepoGitRefsHeadsMain", "+:. => HttpsGithubComAjayKumar9375jsonRepoGitRefsHeadsMain"))
