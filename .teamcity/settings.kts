187fb0e9751ce4b3c602fc25e4d6eb99cc6143a5
Ajay Kumar <ajaya9375@gmail.com>
2024-07-01 08:45:29 +0530

    changes

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -2,6 +2,7 @@ import jetbrains.buildServer.configs.kotlin.*
 
 import DslTesting.DslBuildStep.*
 import DslTesting.buildTypes.DslTesting_BuildTypes
+import vcs.*
 
 import jetbrains.buildServer.configs.kotlin.buildFeatures.perfmon
 import jetbrains.buildServer.configs.kotlin.buildSteps.python
